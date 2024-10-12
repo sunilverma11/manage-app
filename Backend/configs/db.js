f0ca09b1b1e71a533e166ced2efae5e7c5e05e99
sunilverma11 <srksaroya@hotmail.com>
2024-10-12 12:09:33 +0530

    code refectoring

--- Backend/configs/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config()
-connectionUrl = process.env.connection;
-module.exports = () => {
-    return mongoose.connect(connectionUrl)
-    // return mongoose.connect("mongodb+srv://srksaroya:suniltodopassword@cluster0.664mqkn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    //return mongoose.connect("mongodb+srv://srksaroya:sunil@sunilcluster.s9by7.mongodb.net/todoDB?retryWrites=true&w=majority")
-}
-//mongodb+srv://srksaroya:suniltodopassword@cluster0.664mqkn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
